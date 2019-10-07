4436bc0a0b579e90a043c98bbe5350cb546d9bd1
shun-nagayama <allandarp.shun@gmail.com>
2019-10-07 22:23:49 +0900

    bme280 sample code

--- raspberry_pi/bme280_csv.py
+++ raspberry_pi/bme280_csv.py
@@ -80,27 +80,25 @@ def readData():
 
 
 def compensate_P(adc_P):
-	global  t_fine
-	pressure = 0.0
+    global  t_fine
+    pressure = 0.0
+    v1 = (t_fine / 2.0) - 64000.0
+    v2 = (((v1 / 4.0) * (v1 / 4.0)) / 2048) * digP[5]
+    v2 = v2 + ((v1 * digP[4]) * 2.0)
+    v2 = (v2 / 4.0) + (digP[3] * 65536.0)
+    v1 = (((digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8)  + ((digP[1] * v1) / 2.0)) / 262144
+    v1 = ((32768 + v1) * digP[0]) / 32768
 	
-	v1 = (t_fine / 2.0) - 64000.0
-	v2 = (((v1 / 4.0) * (v1 / 4.0)) / 2048) * digP[5]
-	v2 = v2 + ((v1 * digP[4]) * 2.0)
-	v2 = (v2 / 4.0) + (digP[3] * 65536.0)
-	v1 = (((digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8)  + ((digP[1] * v1) / 2.0)) / 262144
-	v1 = ((32768 + v1) * digP[0]) / 32768
-	
-	if v1 == 0:
-		return 0
-	pressure = ((1048576 - adc_P) - (v2 / 4096)) * 3125
-	if pressure < 0x80000000:
-		pressure = (pressure * 2.0) / v1
-	else:
-		pressure = (pressure / v1) * 2
-	v1 = (digP[8] * (((pressure / 8.0) * (pressure / 8.0)) / 8192.0)) / 4096
-	v2 = ((pressure / 4.0) * digP[7]) / 8192.0
-	pressure = pressure + ((v1 + v2 + digP[6]) / 16.0)  
-    
+    if v1 == 0:
+    	return 0
+    pressure = ((1048576 - adc_P) - (v2 / 4096)) * 3125
+    if pressure < 0x80000000:
+    	pressure = (pressure * 2.0) / v1
+    else:
+    	pressure = (pressure / v1) * 2
+    v1 = (digP[8] * (((pressure / 8.0) * (pressure / 8.0)) / 8192.0)) / 4096
+    v2 = ((pressure / 4.0) * digP[7]) / 8192.0
+    pressure = pressure + ((v1 + v2 + digP[6]) / 16.0)
     global press
     press = pressure / 100
 
