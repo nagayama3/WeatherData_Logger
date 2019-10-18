84dd6ce4d9af84820c48f6f019f4e86440dff3b2
shun-nagayama <allandarp.shun@gmail.com>
2019-10-18 20:53:54 +0900

    line_notify

---
+++ raspberry_pi/line_notify.py
@@ -0,0 +1,24 @@
+import requests
+import argparse
+import datetime
+import sys
+
+date = datetime.today()
+filename = datetime.strftime(datetime.now() - timedelta(1), "%Y-%m-%d")
+read_file = read_dir + filename + '.csv'
+save_dir = r"/home/pi/Documents/WeatherData_Logger/raspberry_pi/graphs"
+
+
+def main():
+    url = "https://notify-api.line.me/api/notify"
+    token = "HuJBwmzKyIVh2MX8WJtHu9mmb4Ap3j53WEzXcpiDL68"
+    headers = {"Authorization" : "Bearer" + token}
+
+    message = filename + "の我が家の気象データ"
+    payload = {"message" : message}
+    files = {"imageFile" : open(save_dir + read_file, "rb")}
+
+    line_notify = requests.post(url, data=payload, headers=headers, files=file)
+
+if __name__ == "__main__":
+    main()
