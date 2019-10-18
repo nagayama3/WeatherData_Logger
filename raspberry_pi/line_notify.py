cffd196ed113ecd01e4f695be47479f23b469638
shun-nagayama <allandarp.shun@gmail.com>
2019-10-18 21:18:41 +0900

    [fix]line_notify

--- raspberry_pi/line_notify.py
+++ raspberry_pi/line_notify.py
@@ -2,20 +2,24 @@
 import requests
 import argparse
 import datetime
-import sys
 
 save_dir = r"/home/pi/Documents/WeatherData_Logger/raspberry_pi/graphs/"
 filename = datetime.datetime.strftime(datetime.datetime.now() - datetime.timedelta(1), "%Y-%m-%d")
+pic_path = save_dir + filename + ".png"
 
-def main():
+def main(*args):
     url = "https://notify-api.line.me/api/notify"
     token = "jElh7raXhrBrHKAUomKPX9C07UzH54yoYEqeaK1C2yE"
     headers = {"Authorization" : "Bearer" + token}
 
     message = filename + "の我が家の気象データ"
     payload = {"message" : message}
-    files = {"imageFile" : open(save_dir + filename + ".png", "rb")}
-
-    line_notify = requests.post(url, data=payload, headers=headers, files=files)
+    
+    if len(args) == 0:
+        pass
+    else:
+        files = {"imageFile" : open(save_dir + filename + ".png", "rb")}
+        requests.post(url, data=payload, headers=headers, files=files)
 
 if __name__ == "__main__":
+    main(pic_path)
